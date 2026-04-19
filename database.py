bfd32125329b8755a2411ad4f9c6d1411257c7d3
NguyenLeDuyy <nguyenleduy10122004@gmail.com>
2026-04-19 21:49:54 +0700

    feat: upgrade DB with Alembic and Multi-DB support (Step 2)

--- database.py
+++ database.py
@@ -7,15 +7,22 @@ from sqlalchemy.orm import DeclarativeBase, sessionmaker
 # Load biến từ .env
 load_dotenv()
 
-DATABASE_URL = str(os.getenv("DATABASE_URL", "CHANGE_ME_IN_ENV"))
+DATABASE_URL = str(os.getenv("DATABASE_URL", "postgresql://postgres:123456@localhost:5432/expense_db"))
 
-connect_args={"check_same_thread": False} if DATABASE_URL.startswith("sqlite") else {}
+# SQLite cần check_same_thread
+connect_args = {"check_same_thread": False} if DATABASE_URL.startswith("sqlite") else {}
 
 engine = create_engine(
-    DATABASE_URL, connect_args=connect_args
+    DATABASE_URL, 
+    connect_args=connect_args,
+    pool_pre_ping=True,  # tránh connection chết (quan trọng với Postgres)
 )
 
-SessionLocal = sessionmaker(bind=engine)
+SessionLocal = sessionmaker(
+    bind=engine,
+    autocommit=False,
+    autoflush=False,
+    )
 
 
 class Base(DeclarativeBase):
--- main.py
+++ main.py
@@ -2,12 +2,14 @@ from fastapi import FastAPI
 from database import engine, Base
 from routers.expense import router as expense_router
 from routers.user import router as user_router
+from routers.category import router as category_router
 
 app = FastAPI()
 
 # Create database tables
-Base.metadata.create_all(bind=engine)
+# Base.metadata.create_all(bind=engine)
 
 # Include routers
 app.include_router(expense_router)
 app.include_router(user_router)
+app.include_router(category_router)
